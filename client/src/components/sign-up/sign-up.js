86fd5bb4955d3913b716c709682ac783dfd06531
madjack <kavanokami@gmail.com>
2020-02-27 15:56:57 +0400

    feat: add styling for signUp

--- client/src/components/sign-up/sign-up.js
+++ client/src/components/sign-up/sign-up.js
@@ -3,7 +3,13 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 
 import signUpSchema from './sign-up-schema';
 
+import './sign-up.css';
+
 function SignUp() {
+  const renderError = message => {
+    return <div className='error-message'>{message}</div>;
+  };
+
   return (
     <Formik
       initialValues={{ login: '', password: '' }}
@@ -15,7 +21,7 @@ function SignUp() {
       <Form className='sign-up-form'>
         <label htmlFor='login'>Login</label>
         <Field id='login' name='login' />
-        <ErrorMessage name='login' />
+        <ErrorMessage name='login' render={renderError} />
         <label htmlFor='password'>Password</label>
         <Field id='password' name='password' type='password' />
         <ErrorMessage name='password' />
