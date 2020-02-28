f0329985b83291e8bfdfa9c001330ad82c252e3f
madjack <kavanokami@gmail.com>
2020-02-28 16:37:02 +0400

    feat: handle errors coming from server

--- client/src/components/sign-up/sign-up-schema.js
+++ client/src/components/sign-up/sign-up-schema.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 const signUpSchema = Yup.object({
   login: Yup.string()
-    .min(4, 'Must be at least 3 chars')
+    .min(3, 'Must be at least 3 chars')
     .required('Required'),
   password: Yup.string()
     .min(4, 'Must be at least 4 chars')
--- client/src/components/sign-up/sign-up.js
+++ client/src/components/sign-up/sign-up.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 
 import signUpSchema from './sign-up-schema';
@@ -6,56 +6,73 @@ import signUpSchema from './sign-up-schema';
 import './sign-up.css';
 
 function SignUp() {
+  const [serverInfo, setServerInfo] = useState(null);
+
+  const processServerInfo = serverInfo => {
+    console.log(serverInfo);
+    if (serverInfo.error) {
+      return <div className='error-message'>{serverInfo.message}</div>;
+    }
+  };
+
   const renderError = message => {
     return <div className='error-message'>{message}</div>;
   };
 
   return (
-    <Formik
-      initialValues={{
-        login: '',
-        password: '',
-        confirmPassword: '',
-        email: '',
-        name: '',
-      }}
-      validationSchema={signUpSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        fetch('/sign-up', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify(values),
-        })
-          .then(res => res.json())
-          .then(data => console.log(data));
-      }}
-    >
-      <Form className='sign-up-form'>
-        <label htmlFor='login'>Login</label>
-        <Field id='login' name='login' />
-        <ErrorMessage name='login' render={renderError} />
+    <React.Fragment>
+      <Formik
+        initialValues={{
+          login: '',
+          password: '',
+          confirmPassword: '',
+          email: '',
+          name: '',
+        }}
+        validationSchema={signUpSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(true);
+          fetch('/sign-up', {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify(values),
+          })
+            .then(res => res.json())
+            .then(data => {
+              setServerInfo(data);
+              setSubmitting(false);
+            });
+        }}
+      >
+        <Form className='sign-up-form'>
+          <label htmlFor='login'>Login</label>
+          <Field id='login' name='login' />
+          <ErrorMessage name='login' render={renderError} />
 
-        <label htmlFor='password'>Password</label>
-        <Field id='password' name='password' type='password' />
-        <ErrorMessage name='password' render={renderError} />
+          <label htmlFor='password'>Password</label>
+          <Field id='password' name='password' type='password' />
+          <ErrorMessage name='password' render={renderError} />
 
-        <label htmlFor='confirmPassword'>Confirm Password</label>
-        <Field id='confirmPassword' name='confirmPassword' type='password' />
-        <ErrorMessage name='confirmPassword' render={renderError} />
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <Field id='confirmPassword' name='confirmPassword' type='password' />
+          <ErrorMessage name='confirmPassword' render={renderError} />
 
-        <label htmlFor='email'>Email</label>
-        <Field id='email' name='email' type='email' />
-        <ErrorMessage name='email' render={renderError} />
+          <label htmlFor='email'>Email</label>
+          <Field id='email' name='email' type='email' />
+          <ErrorMessage name='email' render={renderError} />
 
-        <label htmlFor='name'>Name</label>
-        <Field id='name' name='name' />
-        <ErrorMessage name='name' render={renderError} />
+          <label htmlFor='name'>Name</label>
+          <Field id='name' name='name' />
+          <ErrorMessage name='name' render={renderError} />
 
-        <button type='submit'>Submit</button>
-      </Form>
-    </Formik>
+          <button type='submit'>Submit</button>
+
+          {serverInfo && processServerInfo(serverInfo)}
+        </Form>
+      </Formik>
+    </React.Fragment>
   );
 }
 
