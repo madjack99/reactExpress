4298c87fbbb96900ef5abffb99fa0b09ba7bda15
madjack <kavanokami@gmail.com>
2020-03-01 20:07:29 +0400

    feat: show info according to status

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import SignUp from '../sign-up';
 import Home from '../home';
@@ -13,14 +13,22 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        <Route path='/' exact>
+        <Route path='/'>
           <Home loggedUser={loggedUser} />
         </Route>
         <Route path='/sign-up' exact>
-          <SignUp setLoggedUser={setLoggedUser} />
+          {loggedUser ? (
+            <Redirect to='/' />
+          ) : (
+            <SignUp setLoggedUser={setLoggedUser} />
+          )}
         </Route>
         <Route path='/log-in' exact>
-          <LogIn setLoggedUser={setLoggedUser} />
+          {loggedUser ? (
+            <Redirect to='/' />
+          ) : (
+            <LogIn setLoggedUser={setLoggedUser} />
+          )}
         </Route>
       </div>
     </Router>
--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -4,14 +4,26 @@ import { Link } from 'react-router-dom';
 function Home({ loggedUser }) {
   return (
     <div>
-      <h1>Home</h1>
-      <h2>{loggedUser && <span>{`Hi, ${loggedUser}`}</span>}</h2>
+      <h1>Header</h1>
+      <h2>
+        {loggedUser && <span>{`Hi, ${loggedUser}, you're logged in`}</span>}
+      </h2>
       <ul>
+        {loggedUser ? (
+          <h2>Confidential Info</h2>
+        ) : (
+          <>
+            {' '}
+            <li>
+              <Link to='/sign-up'>Sign up</Link>
+            </li>
+            <li>
+              <Link to='/log-in'>Log in</Link>
+            </li>
+          </>
+        )}
         <li>
-          <Link to='/sign-up'>Sign up</Link>
-        </li>
-        <li>
-          <Link to='/log-in'>Log in</Link>
+          <Link to='/'>Home</Link>
         </li>
       </ul>
     </div>
